a3998473ddea25a57e14fe323a87ebb53a27c112
HockIt <mduch028@gmail.com>
2026-05-02 18:31:31 +0200

    Setup Weapon Base Class

---
+++ Source/ActionGame/Weapons/AbilityWeaponBase.cpp
@@ -0,0 +1,27 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilityWeaponBase.h"
+
+
+// Sets default values
+AAbilityWeaponBase::AAbilityWeaponBase()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	WeaponMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("WeaponMesh"));
+	//WeaponMesh->SetupAttachment(GetRootComponent());
+	WeaponMesh->SetCollisionProfileName("NoCollision");
+}
+
+void AAbilityWeaponBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AAbilityWeaponBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
---
+++ Source/ActionGame/Weapons/AbilityWeaponBase.h
@@ -0,0 +1,52 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "AbilityWeaponBase.generated.h"
+
+class UActionAbility;
+class UStaticMeshComponent;
+
+
+
+USTRUCT(BlueprintType)
+struct FWeaponConfig
+{
+	GENERATED_BODY()
+
+	FWeaponConfig() {}
+
+	UPROPERTY(EditDefaultsOnly, Category="Weapon")
+	TArray<TSubclassOf<UActionAbility>> AbilitiesToGrant;
+
+	UPROPERTY(EditDefaultsOnly, Category="Weapon")
+	FName EquippedSocketName;
+
+	UPROPERTY(EditDefaultsOnly, Category="Weapon")
+	TSubclassOf<UAnimInstance> AnimClass;
+};
+
+
+
+UCLASS()
+class ACTIONGAME_API AAbilityWeaponBase : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	AAbilityWeaponBase();
+
+protected:
+	virtual void BeginPlay() override;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category="Weapon")
+	TObjectPtr<UStaticMeshComponent> WeaponMesh;
+
+	UPROPERTY(BlueprintReadWrite, Category="Weapon")
+	FWeaponConfig WeaponConfig;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+};
